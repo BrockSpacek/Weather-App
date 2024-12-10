88dce8537372f7e3d2ea91cb0d4707e7774670ae
BrockSpacek <brockspacek1@gmail.com>
2024-12-10 15:35:07 -0800

    Notes for next days work

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
 import {APIKEY} from './environment.js';
 
+// Notes that I want to add for next day
 
+// Use Switch Statements on the current day and 5 day forecast to change the icons, temps.
+// If its cloudy, rainy, switch to the gray panel, if its night, switch too purple panel, and if its day switch to Yellow Panel
 
 
 // IDs
@@ -32,16 +35,12 @@ let imageDaySeven = document.getElementById('imageDaySeven');
 navigator.geolocation.getCurrentPosition(success);
 
 {
-    coords: {
+    coords: 
+    {
         latitude: 37.9577;
         longitutde: -121.2908;
     }
 }
-
-
-// Functions 
-
-
 function success(position){
     console.log(position)
     console.log("Latitude: " + position.coords.latitude);
@@ -53,6 +52,8 @@ function success(position){
 // function errorFunc(){
 //    console.log(error.message);
 // }
+
+// API 
 
 ApiButton.addEventListener('click', function(event){
     apiCallWeather();
@@ -82,6 +83,10 @@ function apiCallFiveDay(){
     })
 }
 
+
+
+
+// HTML and Local Storage Functions 
 searchBarBtn.addEventListener('click', function(){
 
 
@@ -93,7 +98,7 @@ searchBarBtn.addEventListener('click', function(){
 
 // Workshop Functions that are not finished
 
-import {saveToLocalStorage, getFromLocalStorage, removeFromLocalStorage} from "./localStorage.js";
+import {saveToLocalStorage, getFromLocalStorage, removeFromLocalStorage} from "../localStorage.js";
 
 const inputField = document.getElementById('storageInput');
 const addToStorageBtn = document.getElementById('addToStorageBtn');
@@ -123,7 +128,7 @@ function createElements(){
         let removeButton = document.createElement('button');
         removeButton.type = 'button';
         removeButton.className = "btn btn-danger";
-        removeButton.innerText = "Delete Name";
+        removeButton.innerText = "X";
 
         removeButton.addEventListener('click', function(){
             removeFromLocalStorage(cities);
